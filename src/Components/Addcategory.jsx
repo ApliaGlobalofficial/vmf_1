5f6b33d8d082badeeae847699ddf31e696c1538e
Rajendra Ghongade <105046011+rjghongade@users.noreply.github.com>
2025-05-27 13:59:40 +0530

    .

--- src/Components/Addcategory.jsx
+++ src/Components/Addcategory.jsx
@@ -20,7 +20,7 @@ const AddCategory = () => {
 
   const fetchCategories = async () => {
     try {
-      const response = await axios.get(apiUrl);
+      const response = await axios.get(`${apiUrl}/`);
       setCategories(response.data);
     } catch (error) {
       console.error("Error fetching categories:", error);
